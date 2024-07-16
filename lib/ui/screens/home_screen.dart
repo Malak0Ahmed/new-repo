60b6c2495bb0902f63c863ec4a6a4470ba1c1a17
Your Name <you@example.com>
2024-07-16 14:30:06 +0200

    from c

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -50,85 +50,82 @@ class _HomeScreenState extends State<HomeScreen> {
           textAlign: TextAlign.center,
         ),
       ),
-      body: GridView.builder(
-        itemCount: mylist.length,
-        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
+    body: GridView.builder(
+    itemCount: mylist.length,
+    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+    crossAxisCount: 2,
+    ),
+    itemBuilder: (context, index) {
+    return Card(
+      child:Container(
+        decoration: BoxDecoration(
+          color: Colors.grey.shade200,
+          borderRadius: BorderRadius.circular(10),
         ),
-        itemBuilder: (context, index) {
-          return Card(
-            child:Container(
-              decoration: BoxDecoration(
-                color: Colors.grey.shade200,
-                borderRadius: BorderRadius.circular(10),
-
+        padding: const EdgeInsets.all(8.0),
+        constraints: BoxConstraints(
+          maxHeight: 200, // Set the maximum height for the card
+    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            mylist[index].imageURL != null
+                ? Image.network(
+              height: 70,
+              mylist[index].imageURL!,
+              width: double.infinity,
+              fit:BoxFit.contain,
+            )
+                : Image.asset(
+              'assets/img.jpg',
+              width: double.infinity,
+              fit: BoxFit.cover,
             ),
-            padding: const EdgeInsets.all(8.0),
-              constraints: BoxConstraints(
-                maxHeight: 200, // Set the maximum height for the card
-              ),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.center,
+            Padding(
+              padding: EdgeInsets.all(4.0),
+              child: Text(
 
-              children: [
-                mylist[index].imageURL != null
-                    ? Image.network(
-                  height: 70,
-                  mylist[index].imageURL!,
-                  width: double.infinity,
-                  fit:BoxFit.contain,
-                )
-                    : Image.asset(
-                  'assets/img.jpg',
-                  width: double.infinity,
-                  fit: BoxFit.cover,
+                'name: ${mylist[index].name ?? 'no data'}',
+                maxLines: 1,
+                style: TextStyle(
+                  fontSize: 16,
                 ),
-                Padding(
-                  padding: EdgeInsets.all(4.0),
-                  child: Text(
-                    'name: ${mylist[index].name ?? 'no data'}',
-                    maxLines: 1,
-                    style: TextStyle(
-                        fontSize: 16,
-                    ),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Padding(
-                  padding: EdgeInsets.all(1.0),
-                  child: Text(
-                    'description: ${mylist[index].description ?? 'no data'}',
-                    maxLines: 2,
-                    style: TextStyle(fontSize: 14),
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(1.0),
+              child: Text(
+                'description: ${mylist[index].description ?? 'no data'}',
+                maxLines: 2,
+                style: TextStyle(fontSize: 14),
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                children: [
                   Text(
-                  'price: ${mylist[index].price ?? 'no data'}',
+                    'price: ${mylist[index].price ?? 'no data'}',
                     style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
                   ),
-                      SizedBox(width: 50,),
-                      Icon(
-                          Icons.shop,
-                        size: 22,
-                        color: Colors.deepPurple.shade600,
-                        
-                      ),
-                    ],
-                  ),
+                  SizedBox(width: 50,),
+                  Icon(
+                    Icons.shop,
+                    size: 22,
+                    color: Colors.deepPurple.shade600,),
+                ],
+              ),
 
-                ),
-              ],
             ),
-          ),
-          );
-        },
+          ],
+        ),
       ),
+    );
+    },
+    )
     );
   }
 }
